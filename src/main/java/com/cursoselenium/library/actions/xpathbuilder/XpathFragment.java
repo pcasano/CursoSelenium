efb624d4c3e0ebd4c18d0c31ef50d13f2064c8a1
pcasano <pabloes87@gmail.com>
2021-11-28 12:15:39 +0100

    formatted

--- src/main/java/com/cursoselenium/library/actions/xpathbuilder/XpathFragment.java
+++ src/main/java/com/cursoselenium/library/actions/xpathbuilder/XpathFragment.java
@@ -11,62 +11,67 @@ public class XpathFragment {
         this.sb = sb;
     }
 
-    public XpathFragment child(String value){
+    public XpathFragment child(String value) {
         sb.append("/").append(value);
         return this;
     }
-    public XpathFragment down(String value){
+
+    public XpathFragment down(String value) {
         sb.append("//").append(value);
         return this;
     }
 
-    public XpathFragment attribute(String attribute, String value){
+    public XpathFragment attribute(String attribute, String value) {
         sb.append("[@").append(attribute).append("='").append(value).append("']");
         return this;
     }
 
-    public XpathFragment text(String value){
+    public XpathFragment text(String value) {
         sb.append("[text()='").append(value).append("']");
         return this;
     }
 
-    public XpathFragment textContains(String value){
+    public XpathFragment textContains(String value) {
         sb.append("[contains(text(),'").append(value).append("')]");
         return this;
     }
-    public XpathFragment parent(String value){
+
+    public XpathFragment parent(String value) {
         sb.append("/parent::").append(value);
         return this;
     }
 
-    public XpathFragment attributeContains(String attribute, String value){
+    public XpathFragment attributeContains(String attribute, String value) {
         sb.append("[contains(@").append(attribute).append(",'").append(value).append("')]");
         return this;
     }
-    public XpathFragment attributeStartsWith(String attribute, String value){
+
+    public XpathFragment attributeStartsWith(String attribute, String value) {
         sb.append("[starts-with(@").append(attribute).append(",'").append(value).append("')]");
         return this;
     }
 
-    public XpathFragment textStartsWith(String text){
+    public XpathFragment textStartsWith(String text) {
         sb.append("[starts-with(text(),'").append(text).append("')]");
         return this;
     }
-    public XpathFragment following(String value){
+
+    public XpathFragment following(String value) {
         sb.append("/following::").append(value);
         return this;
     }
-    public XpathFragment id(String value){
+
+    public XpathFragment id(String value) {
         return this.attribute("id", value);
     }
 
 
-    public By toBy(){
+    public By toBy() {
         return By.xpath(sb.toString());
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return sb.toString();
     }
 }
